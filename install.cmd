c9498ef137133e229766bb083f87fd6730ece707
A <666av6@gmail.com>
2025-10-02 15:02:23 +0300

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,6 +1,6 @@
 winget install Obsidian.Obsidian --location "C:\MyPrograms\Obsidian"
 
-winget install -e --id 7zip.7zip --location "C:\MyPrograms\7Zip"
+winget install -e --id 7zip.7zip --location "C:\MyPrograms\7-Zip"
 
 winget install RadolynLabs.AyuGramDesktop --location "C:\MyPrograms\Ayugram"
 
