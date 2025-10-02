17d7124c4e991e000acf9d6277012fe98f04ebde
A <666av6@gmail.com>
2025-10-02 15:03:55 +0300

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -4,6 +4,6 @@ winget install -e --id 7zip.7zip --location "C:\MyPrograms\7-Zip"
 
 winget install RadolynLabs.AyuGramDesktop --location "C:\MyPrograms\Ayugram"
 
-winget install -e --id Git.Git --override "/SILENT /MERGETASKS=runGitAndGui" --location "C:\MyPrograms\Git"
+winget install --id Git.Git --exact --custom "/DIR=\"C:\\MyPrograms\\Git\" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /COMPONENTS=ext,ext\\shellhere,ext\\guihere,gitlfs,assoc,assoc_sh,scalar"
 
 winget install -e --id JetBrains.Toolbox --location "C:\MyPrograms\JetBrainsToolbox"
